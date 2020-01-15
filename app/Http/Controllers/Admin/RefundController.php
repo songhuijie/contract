df1451ad0dde3bf2126d0a85d4a0d8c3ec8f972a
songhuijie <2677060927@qq.com>
2020-01-15 17:51:14 +0800

    s

--- app/Http/Controllers/Admin/RefundController.php
+++ app/Http/Controllers/Admin/RefundController.php
@@ -100,7 +100,7 @@ class RefundController extends Controller
             $appid = $config['appid'];
             $mchid = $config['mch_id'];
             $out_trade_no = $refund->order_number;
-            $out_refund_no = $refund->order_number;
+            $out_refund_no = 'refund_'.uniqid();
             $total_fee = $refund->price;
             $refund_fee = $refund->price;
             $mch_secret = $config['mch_secret'];
